2ecd67a06f36bb2150fa27c159d9a79d38c2d229
Daniel <danielmaskell21@gmail.com>
2018-10-06 22:38:12 +1300

    Initial commit

---
+++ android/src/rhilican/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package rhilican.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import rhilican.game.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
