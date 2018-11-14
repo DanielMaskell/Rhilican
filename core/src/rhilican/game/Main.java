87da41b46acf2932be8dc2e11a8d8b72270ca94f
Daniel <danielmaskell21@gmail.com>
2018-11-14 13:02:16 +1300

    -Added animation (Generic animation at the moment)

--- core/src/rhilican/game/Main.java
+++ core/src/rhilican/game/Main.java
@@ -3,38 +3,78 @@ package rhilican.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.HashMap;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
+
+	Animation animation;
+
+	OrthographicCamera camera;
+
+	ExtendViewport viewport;
+
 	TextureAtlas textureAtlas;
-	Sprite banana;
+
+	private float elapsedTime = 0;
+
+	SpriteBatch batch;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(800, 600, camera);
+
+		textureAtlas = new TextureAtlas(Gdx.files.internal("sprites.txt"));
+
 		batch = new SpriteBatch();
-		img = new Texture("sprite.png");
-		textureAtlas = new TextureAtlas("sprites.txt");
-		banana = textureAtlas.createSprite("Banana");
+		animation = new Animation(1/5f, textureAtlas.getRegions());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		elapsedTime += Gdx.graphics.getDeltaTime();
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		// Draws the animation
+		batch.draw((TextureRegion) animation.getKeyFrame(elapsedTime, true), 0, 0);
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 		textureAtlas.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		viewport.update(width, height, true);
+
+		batch.setProjectionMatrix(camera.combined);
+	}
+
+	private void drawSprite(String name, float x, float y){
+		Sprite sprite = textureAtlas.createSprite(name);
+
+		//Set sprite postion
+		sprite.setPosition(x, y);
+
+		sprite.draw(batch);
 	}
 }
