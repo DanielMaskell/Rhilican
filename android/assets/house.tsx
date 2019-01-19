b9ed425ce12fce46c4840b1dcf68965c607d4969
Daniel <danielmaskell21@gmail.com>
2019-01-19 23:52:38 +1300

    -Map now works. (Placeholder) -Added button image. (Placeholder)

--- android/assets/house.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="house" tilewidth="64" tileheight="64" tilecount="225" columns="15">
- <image source="house.png" width="1000" height="1000"/>
-</tileset>
--- core/src/rhilican/game/Main.java
+++ core/src/rhilican/game/Main.java
@@ -2,55 +2,128 @@ package rhilican.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class Main extends ApplicationAdapter {
+public class Main extends ApplicationAdapter implements InputProcessor {
 
 	final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
 
-	Animation animation;
+	private Map<Integer, TouchInfo> touches = new HashMap<Integer, TouchInfo>();
 
-	OrthographicCamera camera;
+	private Animation animation;
+	private OrthographicCamera camera;
+	private ExtendViewport viewport;
+	private TextureAtlas textureAtlas;
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private int w,h;
+	private String message = "Touch something slaready!";
+	private Texture map;
+	private Texture buttons;
+	private TiledMapRenderer tiledMapRenderer;
 
-	ExtendViewport viewport;
-
-	TextureAtlas textureAtlas;
+	private TiledMap tiledMap;
 
 	private float elapsedTime = 0;
 
-	SpriteBatch batch;
-	
+
+
+	class TouchInfo{
+		public float touchX = 0;
+		public float touchY = 0;
+		public boolean touched = false;
+	}
+
 	@Override
 	public void create () {
+
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+
 		camera = new OrthographicCamera();
-		viewport = new ExtendViewport(800, 600, camera);
+		camera.setToOrtho(false,w,h);
+		camera.update();
+
+		tiledMap = new TmxMapLoader().load("rehilicanTestMap2.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+		viewport = new ExtendViewport(w, h, camera);
 
 		textureAtlas = new TextureAtlas(Gdx.files.internal("sprites.txt"));
 
+
 		batch = new SpriteBatch();
 		animation = new Animation(1/5f, textureAtlas.getRegions());
+
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+
+		buttons = new Texture(Gdx.files.internal("test3.png"));
+
+		Gdx.input.setInputProcessor(this);
+		for(int i = 0; i < 5; i++){
+			touches.put(i, new TouchInfo());
+		}
+
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
 		elapsedTime += Gdx.graphics.getDeltaTime();
+
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+
 		batch.begin();
 
+
+		message = "";
+		for(int i = 0; i < 5; i++){
+			if(touches.get(i).touched)
+				message += "Finger:" + Integer.toString(i) + "touch at:" +
+						Float.toString(touches.get(i).touchX) +
+						"," +
+						Float.toString(touches.get(i).touchY) +
+						"\n";
+		}
+
+		GlyphLayout layout = new GlyphLayout();
+		layout.setText(font, message);
+		float x = w/2 - layout.width/2;
+		float y = h/2 + layout.height/2;
+		font.draw(batch, message, x, y);
+
 		// Draws the animation
-		batch.draw((TextureRegion) animation.getKeyFrame(elapsedTime, true), 0, 0);
+		batch.draw((TextureRegion) animation.getKeyFrame(elapsedTime, true), x-50, y+400);
+
+
+		batch.draw(buttons, 0, 0);
 
 		batch.end();
 	}
@@ -58,8 +131,8 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-
 		textureAtlas.dispose();
+		font.dispose();
 	}
 
 	@Override
@@ -76,5 +149,56 @@ public class Main extends ApplicationAdapter {
 		sprite.setPosition(x, y);
 
 		sprite.draw(batch);
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(pointer < 5){
+			touches.get(pointer).touchX = screenX;
+			touches.get(pointer).touchY = screenY;
+			touches.get(pointer).touched = true;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if(pointer < 5){
+			touches.get(pointer).touchX = 0;
+			touches.get(pointer).touchY = 0;
+			touches.get(pointer).touched = false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
